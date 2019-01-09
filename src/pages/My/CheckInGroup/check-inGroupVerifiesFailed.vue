cf672a031b4f12dc184a796f8e989976bee4f111
ypillar <ypillar@qq.com>
2019-01-10 00:05:47 +0800

    首页加了弹窗，增加了讲师入驻

--- src/pages/My/CheckInGroup/check-inGroupVerifiesFailed.vue
+++ src/pages/My/CheckInGroup/check-inGroupVerifiesFailed.vue
@@ -1,5 +1,41 @@
 <template>
-    <div>
-        签到组核实失败
-    </div>
+  <div class="box">
+    <result-page
+      image="shenheshibai_qs@2x.png"
+      title="很遗憾，只有签到组有扫码权限呦~"
+      desc="长按或扫一扫下方二维码，了解更多精彩课程"
+    >
+      <!-- btn区域 自定义html，想显示什么显示什么 -->
+      <div slot="btn" v-if="btnSucces">
+        <c-button theme="dark" v-on:click="onclick()" size="small">确定</c-button>
+      </div>
+    </result-page>
+   <img class="code" src="../../../assets/imgs/20150318165059_VUt4x.jpeg" v-if="!btnSucces">
+  </div>
 </template>
+<style lang="scss" scoped>
+.code {
+  width: 132px;
+  height: 132px;
+  margin: 0 auto;
+  margin-top: 44px;
+  display: block
+}
+</style>
+
+<script>
+import QRCode from "qrcodejs2";
+export default {
+  name: "Check-inGroupVerifiesFailed",
+  data() {
+    return {
+      number: "123456789", //二维码内容
+      btnSucces: true //是否成功
+    };
+  },
+ 
+  methods: {
+   
+  }
+};
+</script>
--- src/pages/My/Home/home.vue
+++ src/pages/My/Home/home.vue
@@ -40,22 +40,22 @@
 
         <div class="identity">
           <div class="idItem">
-            <div class="backImg">
+            <div :class="{backImg:loginStatus==1,backImgs:loginStatus!=1}">
               <span>弟子</span>
             </div>
           </div>
           <div class="idItem">
-            <div class="backImgs">
+            <div :class="{backImg:loginStatus==2,backImgs:loginStatus!=2}">
               <span>能量</span>
             </div>
           </div>
           <div class="idItem">
-            <div class="backImgs">
+            <div :class="{backImg:loginStatus==3,backImgs:loginStatus!=3}">
               <span>讲师</span>
             </div>
           </div>
           <div class="idItem">
-            <div class="backImgs">
+            <div :class="{backImg:loginStatus==4,backImgs:loginStatus!=4}">
               <span>伙伴</span>
             </div>
           </div>
@@ -74,7 +74,7 @@
               <!-- <small class="notPass">
                 认证未通过
                 <i class="fa fa-angle-right" aria-hidden="true"></i>
-              </small> -->
+              </small>-->
             </div>
           </div>
         </div>
@@ -101,24 +101,82 @@
     </div>
     <!-- 内容区 -->
     <div class="padBottom">
+      <!-- 我的管理 -->
+      <div class="myManagement" v-if="loginStatus==4">
+        <div class="leftCer">
+          <span>我的管理</span>
+          <p>旗下合作伙伴信息</p>
+        </div>
+        <div class="rightCer">
+          <small :class="{notPass:pass}">
+            前往查看
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
+          </small>
+          <!-- <small class="notPass">
+                认证未通过
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+          </small>-->
+        </div>
+      </div>
       <!-- 签到组 -->
-     <div class="orderBox padding ">
+      <div class="orderBox padding" v-if="loginStatus==3">
         <div class="titBox">
-          <span class="orderTit">我的签到组</span>         
+          <span class="orderTit">我的签到组</span>
         </div>
         <div class="distinguish">
           <div class="item">
-            <img src="../../../assets/imgs/saoyisao.png" alt="">
+            <img src="../../../assets/imgs/saoyisao.png" alt>
             <span>扫码识别</span>
-          </div> <div class="item">
-            <img src="../../../assets/imgs/shenfenrenzheng_mine.png" alt="">
+          </div>
+          <div class="item">
+            <img src="../../../assets/imgs/shenfenrenzheng_mine.png" alt>
             <span>证件识别</span>
           </div>
         </div>
-        
+         <div class="orderNav">
+          <router-link class="menuItem" to="keyframes">
+            <div class="menuItemIcon widAuto">12000</div>
+            <div class="itemText">已签到人数</div>
+          </router-link>
+          <router-link class="menuItem" to="keyframes">
+            <div class="menuItemIcon widAuto">14000</div>
+            <div class="itemText">总扫码人数</div>
+          </router-link>
+          <router-link class="menuItem" to="keyframes">
+            <div class="menuItemIcon widAuto">200</div>
+            <div class="itemText">我扫码人数</div>
+          </router-link>
+        </div>
+      </div>
+      <!-- 我的签到——弟子 -->
+      <div class="my-check-in padding" v-if="loginStatus==1">
+        <div class="check-in-center">
+          <p>签到中心</p>
+          <small>以下信息仅作为签到处认证使用，课程结束后将自动消失</small>
+        </div>
+        <div class="checkBox">
+          <ul>
+            <li>
+              <img src="../../../assets/imgs/20150318165059_VUt4x.jpeg" alt="头像照片" v-on:click="popupVisible=true">
+              <div>
+                <p>真实姓名：王某某</p>
+                <p>手机号码：15454454567</p>
+                <p>身份证号：123456789012345678</p>
+              </div>
+            </li>
+            <li>
+              <img src="../../../assets/imgs/20150318165059_VUt4x.jpeg" alt="头像照片" v-on:click="popupCode=true">
+              <div>
+                <p>状态：未扫码</p>
+                <p>明细：经营能量</p>
+                <p>签到时间：2019-03-09 21:00:00</p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
       <!-- 全部订单 -->
-      <div class="orderBox padding ">
+      <div class="orderBox padding">
         <div class="titBox">
           <span class="orderTit">我的订单</span>
           <small>
@@ -135,8 +193,8 @@
           </router-link>
           <router-link class="menuItem" to="keyframes">
             <div class="menuItemIcon">
-              
-              <div class="num">1</div><!-- 消息条数 -->
+              <div class="num">1</div>
+              <!-- 消息条数 -->
               <img src="../../../assets/imgs/daifukuan.png">
             </div>
             <div class="itemText">待付款</div>
@@ -171,15 +229,15 @@
         <div class="orderNav">
           <router-link class="menuItem" to="keyframes">
             <div class="menuItemIcon">--</div>
-            <div class="itemText">待审核</div>
+            <div class="itemText">未使用</div>
           </router-link>
           <router-link class="menuItem" to="keyframes">
             <div class="menuItemIcon">--</div>
-            <div class="itemText">待付款</div>
+            <div class="itemText">已送出</div>
           </router-link>
           <router-link class="menuItem" to="keyframes">
             <div class="menuItemIcon">--</div>
-            <div class="itemText">待签到</div>
+            <div class="itemText">已使用</div>
           </router-link>
         </div>
       </div>
@@ -202,6 +260,18 @@
         </ul>
       </div>
     </div>
+     <modal :visible="popupVisible" v-on:close="popupVisible=false">
+        <div slot="content">
+          <div>
+           <img style="width:100%;height:100%;display:block;" src="../../../assets/imgs/20150318165059_VUt4x.jpeg" alt="">
+          </div>
+        </div> 
+      </modal>
+       <modal :visible="popupCode" v-on:close="popupCode=false">
+        <div slot="content">
+         <img style="width:100%;height:100%;display:block;" src="../../../assets/imgs/20150318165059_VUt4x.jpeg" alt="">
+        </div> 
+      </modal>
     <tabbar :number="2"></tabbar>
   </div>
 </template>
@@ -348,6 +418,7 @@
       }
     }
   }
+
   //我的认证
   .myCertification {
     width: 100%;
@@ -360,6 +431,7 @@
       display: inline-block;
       margin-top: 10px;
       margin-left: 10px;
+      font-family: PingFangSC-Medium;
       span {
         font-size: 1.6rem;
         color: #333;
@@ -368,6 +440,7 @@
         font-size: 12px;
         color: #999;
         margin-top: 3px;
+        font-family: PingFangSC-Regular;
       }
     }
     .rightCer {
@@ -377,9 +450,126 @@
       small {
         font-size: 1.4rem;
         color: #666;
+        font-family: PingFangSC-Regular;
       }
-      .notPass{
-        color: #FF622B !important;
+      .notPass {
+        color: #ff622b !important;
+      }
+    }
+  }
+}
+
+//我的管理
+.myManagement {
+  width: 100%;
+  height: 62px;
+  background: #fff;
+  border-radius: 4px;
+  .leftCer {
+    display: inline-block;
+    margin-top: 10px;
+    margin-left: 10px;
+    font-family: PingFangSC-Medium;
+    span {
+      font-size: 1.6rem;
+      color: #333;
+    }
+    p {
+      font-size: 12px;
+      color: #999;
+      margin-top: 3px;
+      font-family: PingFangSC-Regular;
+    }
+  }
+  .rightCer {
+    float: right;
+    margin-top: 21px;
+    margin-right: 24px;
+    small {
+      font-size: 1.4rem;
+      color: #666;
+      font-family: PingFangSC-Regular;
+    }
+  }
+}
+//我的签到
+.my-check-in {
+  margin-top: 20px;
+  .check-in-center {
+    p {
+      width: auto;
+      height: 22px;
+      font-size: 1.6rem;
+      font-family: PingFangSC-Medium;
+      font-weight: 500;
+      color: rgba(51, 51, 51, 1);
+      line-height: 22px;
+    }
+    small {
+      display: block;
+      width: auto;
+      font-size: 1.2rem;
+      font-family: PingFangSC-Regular;
+      font-weight: 400;
+      color: #999;
+      line-height: 17px;
+      margin-top: 3px;
+    }
+  }
+  .checkBox {
+    margin-top: 10px;
+    ul {
+      li {
+        margin-bottom: 20px;
+        img {
+          display: block;
+          width: 30%;
+
+          float: left;
+        }
+        div {
+          float: left;
+          margin-left: 14px;
+          width: calc(70% - 14px);
+          p:first-child {
+            width: auto;
+            height: 21px;
+            font-size: 1.6rem;
+            font-family: PingFangSC-Medium;
+            font-weight: 500;
+            color: #333;
+            line-height: 21px;
+            margin-top: 12px;
+          }
+          p:nth-child(2) {
+            width: auto;
+            height: 21px;
+            font-size: 1.2rem;
+            font-family: PingFangSC-Regular;
+            font-weight: 400;
+            color: #666;
+            line-height: 21px;
+            margin-top: 15px;
+          }
+          p:last-child {
+            width: 215px;
+            height: 21px;
+            font-size: 1.2rem;
+            font-family: PingFangSC-Regular;
+            font-weight: 400;
+            color: #666;
+            line-height: 21px;
+            margin-top: 5px;
+          }
+        }
+      }
+      li:after {
+        clear: both;
+        content: ".";
+        display: block;
+        width: 0;
+        height: 0;
+        visibility: hidden;
       }
     }
   }
@@ -403,6 +593,7 @@
       font-size: 16px;
       color: #333;
       font-weight: 500;
+      font-family: PingFangSC-Medium;
     }
     small {
       float: right;
@@ -440,6 +631,9 @@
           color: #fff;
         }
       }
+      .widAuto{
+        width: auto !important;
+      }
 
       .itemText {
         font-size: 1.2rem;
@@ -449,28 +643,34 @@
     }
   }
   //签到组
-  .distinguish{
+  .distinguish {
     display: flex;
-    justify-content:space-between;
-   .item{
-     margin-top: 14px;
-     width: 160px;
-     border:1px solid #2CAD88;
-     height: 40px;
-     border-radius:4px;
-     line-height: 40px;
-     text-align: center;
-     img{
-          vertical-align: middle;
-    margin-top: -4px;
-     }
-     span{
-       font-size: 1.6rem;
-       color: #666;
-       font-family:PingFangSC-Regular;
-       margin-left: 5px;
-     }
-   }
+    justify-content: space-between;
+    .item {
+      margin-top: 14px;
+      width: 160px;
+      border: 1px solid #2cad88;
+      height: 40px;
+      border-radius: 4px;
+      line-height: 40px;
+      text-align: center;
+      img {
+        vertical-align: middle;
+        margin-top: -4px;
+      }
+      span {
+        font-size: 1.6rem;
+        color: #666;
+        font-family: PingFangSC-Regular;
+        margin-left: 5px;
+      }
+    }
+    .item:first-child {
+      margin-right: 5px;
+    }
+    .item:last-child {
+      margin-left: 5px;
+    }
   }
 }
 //列表
@@ -537,14 +737,23 @@
 }
 </style>
 <script>
+
 export default {
-  name: "homesignln",
+  name: "Home",
   data() {
     return {
       selected: "",
       login: true, //是否登录
-      pass:false,//认证是否通过
+      pass: false, //认证是否通过
+      loginStatus:1,//登录身份1弟子2能量3讲师4伙伴
+      popupVisible:false,
+      popupCode:false
     };
+  },
+  methods:{
+    selcet(){
+
+    }
   }
 };
 </script>
